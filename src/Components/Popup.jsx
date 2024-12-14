af705a867ed81fb253805db04c432561dee31b78
Shahd Elghamry <139113340+Shahd-elghamry@users.noreply.github.com>
2024-12-14 10:26:02 +0200

    moved one css file to another

--- src/Components/Popup.jsx
+++ src/Components/Popup.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import './PopupStyle.css';
 
+// eslint-disable-next-line no-unused-vars
 const BASE_URL = 'http://127.0.0.1:5005';
 
 const Popup = ({ type, closePopup, token }) => {
